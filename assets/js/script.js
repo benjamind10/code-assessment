ecd9142d4663fcc16b62713d0a967ecf7918cacb
Ben <benjamind10@pm.me>
2021-12-16 15:31:39 -0500

    working function that renders the right multiple choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var mainButton = document.getElementById('main-button');
 var mTitle = document.getElementById('mTitle');
 var mBody = document.getElementById('body');
+var form = document.querySelector('form');
+var mChoices = document.querySelector('input');
 
 var mainObj = [
   {
@@ -9,8 +11,12 @@ var mainObj = [
   },
   {
     title: 'Commonly used data types DO NOT include:',
-    options: ['strings', 'booleans', 'alerts', 'options'],
+    options: ['strings', 'booleans', 'alerts', 'numbers'],
     answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed with?',
+    options: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
   },
 ];
 
@@ -19,11 +25,22 @@ mBody.textContent = mainObj[0].body;
 
 mainButton.addEventListener('click', function () {
   mTitle.textContent = mainObj[1].title;
+  mainButton.style.display = 'none';
+  makeButton(mainObj[1].options);
 });
 
-function makeButton() {
-  var choices1 = mainObj[1].options;
-  for (var i = 0; i < choices1.length; i++) {
-    console.log(choices1[i]);
+form.addEventListener('click', function (e) {
+  console.log(e);
+});
+
+function makeButton(choices) {
+  mBody.textContent = '';
+
+  for (var i = 0; i < choices.length; i++) {
+    var btnChoice = document.createElement('input');
+    btnChoice.className = 'ui button';
+    btnChoice.type = 'button';
+    btnChoice.value = choices[i];
+    form.appendChild(btnChoice);
   }
 }
