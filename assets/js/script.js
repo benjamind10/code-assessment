82dff0d95a266a7ed5120bd85c0bdf6a06f4accc
Ben <benjamind10@pm.me>
2021-12-16 23:27:09 -0500

    working function that validates answers and prints new question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,23 @@ var startPage = document.getElementById('start-page');
 var mainBtn = document.getElementById('btn-start');
 var questionEl = document.getElementById('questions');
 var buttonsEl = document.getElementById('buttons');
-var quizPage = document.getElementById('quiz-page');
+var gamePage = document.getElementById('game-page');
+var results = document.getElementById('result');
+var btnA = document.getElementById('a');
+var btnB = document.getElementById('b');
+var btnC = document.getElementById('c');
+var btnD = document.getElementById('d');
 
 var mainObj = [
   {
     title: 'Commonly used data types DO NOT include ________:',
-    options: ['strings', 'booleans', 'alerts', 'numbers'],
-    answer: 'alerts',
+    options: ['strings', 'alerts', 'booleans', 'numbers'],
+    answer: 'b',
   },
   {
     title: 'The condition in an if / else statement is enclosed with ________.',
     options: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-    answer: 'parenthesis',
+    answer: 'c',
   },
   {
     title: 'Arrays in JavaScript can be used to store ________.',
@@ -23,49 +28,70 @@ var mainObj = [
       'booleans',
       'all of the above',
     ],
-    answer: 'all of the above',
+    answer: 'c',
   },
   {
     title:
       'String values must be enclosed within ________ when being assigned to variables',
     options: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-    answer: 'quotes',
+    answer: 'c',
   },
   {
     title:
       'A very useful tool during development and debugging for printing content to the debugger is ________.',
     options: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
-    answer: 'console.log',
+    answer: 'd',
   },
 ];
 
 var currentIndex = 0;
 var lastIndex = mainObj.length;
+var score = 0;
 
 mainBtn.addEventListener('click', function () {
-  debugger;
   gameStart();
 });
 
 function gameStart() {
   startPage.style.display = 'none';
-  genQuestions();
+  genQuestion();
 }
 
-function genQuestions() {
-  quizPage.style.display = 'flex';
-  questionEl.innerHTML = '<p>' + mainObj[currentIndex].title + '<p>';
-  quizPage.appendChild(questionEl);
-  makeButtons();
-}
+function genQuestion() {
+  gamePage.style.display = 'block';
+  var activeQ = mainObj[currentIndex];
 
-function makeButtons() {
-  var choices = mainObj[currentIndex].options;
-  for (var i = 0; i < choices.length; i++) {
-    var btnChoice = document.createElement('button');
-    btnChoice.className = 'ui button';
-    btnChoice.type = 'button';
-    btnChoice.value = choices[i];
-    buttonsEl.appendChild(btnChoice);
+  if (currentIndex === lastIndex) {
+    return gameOver();
   }
+  questionEl.innerHTML = '<h2>' + activeQ.title + '</h2>';
+  btnA.innerHTML = activeQ.options[0];
+  btnB.innerHTML = activeQ.options[1];
+  btnC.innerHTML = activeQ.options[2];
+  btnD.innerHTML = activeQ.options[3];
+}
+
+function validate(answer) {
+  var final = mainObj[currentIndex].answer;
+  var isChecked = document.createElement('p');
+
+  if (answer === final && currentIndex != lastIndex) {
+    score++;
+    currentIndex++;
+    isChecked.textContent = 'Correct';
+    results.appendChild(isChecked);
+    genQuestion();
+  } else if (answer != final && currentIndex != lastIndex) {
+    isChecked.textContent = 'Wrong!';
+    results.appendChild(isChecked);
+    currentIndex++;
+    genQuestion();
+  } else {
+    gameOver();
+  }
+}
+
+function gameOver() {
+  gamePage.style.display = 'none';
+  return score;
 }
